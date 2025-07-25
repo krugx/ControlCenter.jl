30d60b820927927f00708fe06d2fc9e33923c010
Malte Krug <malte.krug@uni-ulm.de>
2025-07-25 18:08:04 +0200

    added the exports for HEOM and Control stuff

--- src/ControlCenter.jl
+++ src/ControlCenter.jl
@@ -8,4 +8,18 @@ include("structs.jl")
 include("grape.jl")
 include("helper.jl")
 
+# FPHEOM headers
+export build_heom_structure
+export HEOMOperator
+export HEOMPropagator
+export check_stability
+export bary_fit
+
+# Control Suite headers
+export Generator
+export Trajectory
+export ControlProblem
+export Config, Policy, Parametrization, pulse
+export grape_control
+
 end
