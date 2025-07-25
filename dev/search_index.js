c4a248de891a0c4ef84f6d4c4e2d8ffd0a27b40b
Documenter.jl <documenter@juliadocs.github.io>
2025-07-25 15:29:34 +0000

    build based on 67fa307

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ControlCenter","page":"Home","title":"ControlCenter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ControlCenter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
